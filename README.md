2cd893369a126a72da80083d55f1511b824e06a0
ILikeTea <ybingcheng@gmail.com>
2022-04-28 00:32:57 +0800

    Feat/erc20 (#2)
    * test mint
    
    * forge install: forge-std
    
    * Add owner and test
    
    * forge install: openzeppelin-contracts
    
    * rename

---
+++ README.md
@@ -0,0 +1,6 @@
+To test
+
+```
+$ forge test -vvvv --gas-report
+```
+vvvv (very very verbose)
---
+++ src/GoodToken.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "solmate/tokens/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+/**
+ * @title GoodToken
+ * @notice Basic ERC20 implementation
+ * @author BlockByBlock
+ **/
+contract GoodToken is ERC20, Ownable {
+    struct Minting {
+        uint256 time;
+        uint256 amount;
+    }
+
+    Minting public lastMint;
+    uint256 private constant MINTING_PERIOD = 24 hours;
+    uint256 private constant MINTING_INCREASE = 15000;
+    uint256 private constant MINTING_PRECISION = 1e5;
+    constructor() ERC20("Good Token", "GUDGOOD", 18) {}
+
+    /**
+     * @dev Mint tokens
+     * @param to The recipient of the tokens
+     * @param amount The amount of tokens being mintede
+     **/
+    function mint(address to, uint256 amount) external onlyOwner {
+        // Limits the amount minted per period to a convergence function, with the period duration restarting on every mint
+        uint256 totalMintedAmount = uint256(lastMint.time < (block.timestamp - MINTING_PERIOD) ? 0 : lastMint.amount) + amount;
+        require(totalSupply == 0 || totalSupply * (MINTING_INCREASE) / MINTING_PRECISION >= totalMintedAmount);
+
+        lastMint.time = block.timestamp;
+        lastMint.amount = totalMintedAmount;
+
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Burn tokens
+     * @param from The origin of the tokens
+     * @param amount The amount of tokens to burn
+     **/
+    function burn(address from, uint256 amount) external {
+        require(amount <= balanceOf[msg.sender], "MIM: not enough");
+        _burn(from, amount);
+    }
+}
