4bf4b04d869a8823a1df7c8cfc069c8bd0d9142c
blockbyblock <ybingcheng@gmail.com>
2022-04-27 17:44:01 +0800

    Add owner and test

--- src/Contract.sol
+++ src/Contract.sol
@@ -3,11 +3,20 @@ pragma solidity 0.8.10;
 
 import "solmate/tokens/ERC20.sol";
 
+error Unauthorized();
+
 contract Contract is ERC20 {
-    constructor() ERC20("Good Token", "GUDGOOD", 18) {}
+    // todo: mutable owner
+    address public immutable owner;
+    constructor() ERC20("Good Token", "GUDGOOD", 18) {
+        owner = msg.sender;
+    }
 
     // danger
     function mint(address to, uint256 amount) external {
+        if (msg.sender != owner) {
+            revert Unauthorized();
+        }
         _mint(to, amount);
     }
 }
