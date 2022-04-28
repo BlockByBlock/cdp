6add50a1930efa2c751557f9be873cfc191235fd
blockbyblock <ybingcheng@gmail.com>
2022-04-28 17:17:51 +0800

    add borrow and payback

--- src/MakeGood.sol
+++ src/MakeGood.sol
@@ -21,6 +21,9 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
     string public baseURI;
     uint256 public vaultCount;
     uint256 public tokenPeg;
+    uint256 public treasury;
+    uint256 public closingFee;
+    uint256 public openingFee;
     uint256 public minimumCollateralPercentage;
 
     mapping(uint256 => uint256) public vaultCollateral;
@@ -30,6 +33,8 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
     event DestroyVault(uint256 vaultID);
     event DepositCollateral(uint256 vaultID, uint256 amount);
     event WithdrawCollateral(uint256 vaultID, uint256 amount);
+    event BorrowToken(uint256 vaultID, uint256 amount);
+    event PayBackToken(uint256 vaultID, uint256 amount, uint256 closingFee);
 
     IERC20 public immutable collateral;
     IERC20 public immutable goodToken;
@@ -45,6 +50,9 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
         baseURI = _baseURI;
         collateral = _collateral;
         goodToken = _goodToken;
+
+        closingFee = 50; // 0.5%
+        openingFee = 0; // 0.0%
     }
 
     /**
@@ -57,6 +65,13 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
     }
 
     /**
+     * @notice the max debt that can be borrowed as good token from the maker
+     */
+    function getDebtCeiling() public view returns (uint256) {
+        return goodToken.balanceOf(address(this));
+    }
+
+    /**
      * @notice NFT URI
      * @param vaultID ID of Vault
      */
@@ -65,17 +80,16 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, vaultID.toString())) : "";
     }
 
-    function getTokenPriceSource() public view returns (uint256){
+    function getTokenPriceSource() public view returns (uint256) {
         return tokenPeg;
     }
 
-    function getEthPriceSource() public view returns (uint256){
+    function getEthPriceSource() public view returns (uint256) {
         int256 price = ethOracleFeed.getLatestPrice();
         return uint256(price); // brings it back to 18.
     }
 
     function calculateCollateralProperties(uint256 _collateral, uint256 _debt) private view returns (uint256, uint256) {
-
         assert(getEthPriceSource() != 0);
         assert(getTokenPriceSource() != 0);
 
@@ -94,11 +108,10 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
         return (collateralValueTimes100, debtValue);
     }
 
-
     function isValidCollateral(uint256 _collateral, uint256 debt) private view returns (bool) {
         (uint256 collateralValueTimes100, uint256 debtValue) = calculateCollateralProperties(_collateral, debt);
 
-        uint256 collateralPercentage = collateralValueTimes100 * (10 ** 10) / debtValue;
+        uint256 collateralPercentage = (collateralValueTimes100 * (10**10)) / debtValue;
 
         return collateralPercentage >= minimumCollateralPercentage;
     }
@@ -127,7 +140,7 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
     function destroyVault(uint256 vaultID) external onlyVaultOwner(vaultID) nonReentrant {
         require(vaultDebt[vaultID] == 0, "Vault has outstanding debt");
 
-        if(vaultCollateral[vaultID] != 0) {
+        if (vaultCollateral[vaultID] != 0) {
             // withdraw leftover collateral
             collateral.transfer(_ownerOf[vaultID], vaultCollateral[vaultID]);
         }
@@ -146,7 +159,6 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
      * @param amount amount of collateral
      */
     function depositCollateral(uint256 vaultID, uint256 amount) external {
-
         collateral.transferFrom(msg.sender, address(this), amount);
 
         uint256 newCollateralAmt = vaultCollateral[vaultID] + amount;
@@ -168,13 +180,65 @@ contract MakeGood is ERC721, Ownable, ReentrancyGuard {
 
         uint256 newCollateralAmt = vaultCollateral[vaultID] - amount;
 
-        if(vaultDebt[vaultID] != 0) {
-            require(isValidCollateral(newCollateralAmt, vaultDebt[vaultID]), "Withdrawal would put vault below minimum collateral percentage");
+        if (vaultDebt[vaultID] != 0) {
+            require(
+                isValidCollateral(newCollateralAmt, vaultDebt[vaultID]),
+                "Withdrawal would put vault below minimum collateral percentage"
+            );
         }
 
         vaultCollateral[vaultID] = newCollateralAmt;
         collateral.transfer(msg.sender, amount);
 
         emit WithdrawCollateral(vaultID, amount);
+    }
+
+    /**
+     * @notice borrow good token
+     * @param vaultID Vault ID
+     * @param amount Amount of good token to borrow
+     */
+    function borrowToken(uint256 vaultID, uint256 amount) external onlyVaultOwner(vaultID) {
+        require(amount > 0, "Must borrow non-zero amount");
+        require(amount <= getDebtCeiling(), "borrowToken: Cannot mint over available supply.");
+
+        uint256 newDebt = vaultDebt[vaultID] + amount;
+
+        assert(newDebt > vaultDebt[vaultID]);
+
+        require(
+            isValidCollateral(vaultCollateral[vaultID], newDebt),
+            "Borrow would put vault below minimum collateral percentage"
+        );
+
+        vaultDebt[vaultID] = newDebt;
+
+        // goodToken
+        goodToken.transfer(msg.sender, amount);
+
+        emit BorrowToken(vaultID, amount);
+    }
+
+    /**
+     * @notice repay good token
+     * @param vaultID Vault ID
+     * @param amount Amount of good token to repay
+     */
+    function payBackToken(uint256 vaultID, uint256 amount) external {
+        require(goodToken.balanceOf(msg.sender) >= amount, "Token balance too low");
+        require(vaultDebt[vaultID] >= amount, "Vault debt less than amount to pay back");
+
+        uint256 _closingFee = (amount * closingFee * getTokenPriceSource()) /
+            (getEthPriceSource() * 10000) /
+            1000000000;
+
+        //mai
+        goodToken.transferFrom(msg.sender, address(this), amount);
+
+        vaultDebt[vaultID] = vaultDebt[vaultID] - amount;
+        vaultCollateral[vaultID] = vaultCollateral[vaultID] - _closingFee;
+        vaultCollateral[treasury] = vaultCollateral[treasury] + _closingFee;
+
+        emit PayBackToken(vaultID, amount, _closingFee);
     }
 }
